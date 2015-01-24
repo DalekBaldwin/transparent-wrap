f919e8fa75ab59cb02a833e621dfa77891ffe02c
DalekBaldwin <DalekBaldwin@users.noreply.github.com>
2015-01-23 21:28:59 -0800

    test all possible combinations

--- test/original.lisp
+++ test/original.lisp
@@ -2,44 +2,135 @@
   (:use :cl)
   (:export
    #:no-params
+   #:no-params-aux
    #:required
+   #:required-aux
    #:required-optional
+   #:required-optional-aux
    #:required-optional-rest
+   #:required-optional-rest-aux
    #:required-optional-rest-key
+   #:required-optional-rest-key-aux
    #:required-optional-key
+   #:required-optional-key-aux
    #:required-key
-   #:required-rest-key))
+   #:required-key-aux
+   #:required-rest
+   #:required-rest-aux
+   #:required-rest-key
+   #:required-rest-key-aux))
 
 (in-package :transparent-wrap-test.original)
 
 (defun no-params () nil)
 
-(defun required (a b)
+(defun no-params-aux (&aux a)
+  a)
+
+(defun required
+    (a b)
   (list a b))
 
-(defun required-optional (a b &optional c (d :d-default) (e :e-default e-supplied))
+(defun required-aux
+    (a b
+     &aux (c (list a b)))
+  (list a b c))
+
+(defun required-optional
+    (a b
+     &optional c (d :d-default) (e :e-default e-supplied))
   (list a b c d e e-supplied))
 
-(defun required-optional-rest (a b &optional c (d :d-default) (e :e-default e-supplied) &rest f)
+(defun required-optional-aux
+    (a b
+     &optional c (d :d-default) (e :e-default e-supplied)
+     &aux (f (list a b c d e e-supplied)))
   (list a b c d e e-supplied f))
 
-(defun required-optional-rest-key (a b &optional c (d :d-default) (e :e-default e-supplied)
-                                   &rest rest
-                                   &key f (g :g-default) (h :h-default h-supplied)
-                                ((:z i)) ((:y j) :j-default) ((:x k) :k-default k-supplied))
+(defun required-optional-rest
+    (a b
+     &optional c (d :d-default) (e :e-default e-supplied)
+     &rest f)
+  (list a b c d e e-supplied f))
+
+(defun required-optional-rest-aux
+    (a b
+     &optional c (d :d-default) (e :e-default e-supplied)
+     &rest f
+     &aux (g (list a b c d e e-supplied f)))
+  (list a b c d e e-supplied f g))
+
+(defun required-optional-rest-key
+    (a b
+     &optional c (d :d-default) (e :e-default e-supplied)
+     &rest rest
+     &key f (g :g-default) (h :h-default h-supplied)
+       ((:z i)) ((:y j) :j-default)
+       ((:x k) :k-default k-supplied))
   (list a b c d e e-supplied f g h h-supplied i j k k-supplied rest))
 
-(defun required-optional-key (a b &optional c (d :d-default) (e :e-default e-supplied)
-                              &key f (g :g-default) (h :h-default h-supplied)
-                                ((:z i)) ((:y j) :j-default) ((:x k) :k-default k-supplied))
+(defun required-optional-rest-key-aux
+    (a b
+     &optional c (d :d-default) (e :e-default e-supplied)
+     &rest rest
+     &key f (g :g-default) (h :h-default h-supplied)
+       ((:z i)) ((:y j) :j-default)
+       ((:x k) :k-default k-supplied)
+     &aux (l (list a b c d e e-supplied f g h
+                   h-supplied i j k k-supplied rest)))
+  (list a b c d e e-supplied f g h h-supplied i j k k-supplied rest l))
+
+(defun required-optional-key
+    (a b
+     &optional c (d :d-default) (e :e-default e-supplied)
+     &key f (g :g-default) (h :h-default h-supplied)
+       ((:z i)) ((:y j) :j-default) ((:x k) :k-default k-supplied))
   (list a b c d e e-supplied f g h h-supplied i j k k-supplied))
 
+(defun required-optional-key-aux
+    (a b
+     &optional c (d :d-default) (e :e-default e-supplied)
+     &key f (g :g-default) (h :h-default h-supplied)
+       ((:z i)) ((:y j) :j-default) ((:x k) :k-default k-supplied)
+     &aux (l (list a b c d e e-supplied f g h
+                   h-supplied i j k k-supplied)))
+  (list a b c d e e-supplied f g h h-supplied i j k k-supplied l))
 
-
-(defun required-key (a b &key c (d :d-default) (e :e-default e-supplied)
-                           ((:z f)) ((:y g) :g-default) ((:x h) :h-default h-supplied))
+(defun required-key
+    (a b
+     &key c (d :d-default) (e :e-default e-supplied)
+       ((:z f)) ((:y g) :g-default) ((:x h) :h-default h-supplied))
   (list a b c d e e-supplied f g h h-supplied))
 
-(defun required-rest-key (a b &rest rest &key c (d :d-default) (e :e-default e-supplied)
-                           ((:z f)) ((:y g) :g-default) ((:x h) :h-default h-supplied))
+(defun required-key-aux
+    (a b
+     &key c (d :d-default) (e :e-default e-supplied)
+       ((:z f)) ((:y g) :g-default) ((:x h) :h-default h-supplied)
+     &aux (i (list a b c d e e-supplied f g h h-supplied)))
+  (list a b c d e e-supplied f g h h-supplied i))
+
+(defun required-rest
+    (a b
+     &rest rest)
+  (list a b rest))
+
+(defun required-rest-aux
+    (a b
+     &rest rest
+     &aux (c (list a b rest)))
+  (list a b rest c))
+
+(defun required-rest-key
+    (a b
+     &rest rest
+     &key c (d :d-default) (e :e-default e-supplied)
+       ((:z f)) ((:y g) :g-default) ((:x h) :h-default h-supplied))
   (list a b c d e e-supplied f g h h-supplied rest))
+
+(defun required-rest-key-aux
+    (a b
+     &rest rest
+     &key c (d :d-default) (e :e-default e-supplied)
+       ((:z f)) ((:y g) :g-default) ((:x h) :h-default h-supplied)
+     &aux (i (list a b c d e e-supplied f g h h-supplied rest)))
+  (list a b c d e e-supplied f g h h-supplied rest i))
--- test/transparent-wrap-test.lisp
+++ test/transparent-wrap-test.lisp
@@ -10,11 +10,23 @@
     (transparent-wrap-test.original:no-params)
     (transparent-wrap-test.wrapping:no-params))))
 
+(deftest test-no-params-aux ()
+  (is
+   (equal
+    (transparent-wrap-test.original:no-params-aux)
+    (transparent-wrap-test.wrapping:no-params-aux))))
+
 (deftest test-required ()
   (is
    (equal
     (transparent-wrap-test.original:required 1 2)
     (transparent-wrap-test.wrapping:required 1 2))))
+
+(deftest test-required-aux ()
+  (is
+   (equal
+    (transparent-wrap-test.original:required-aux 1 2)
+    (transparent-wrap-test.wrapping:required-aux 1 2))))
 
 (deftest test-required-optional ()
   (is
@@ -34,6 +46,24 @@
     (transparent-wrap-test.original:required-optional 1 2 3 4 5)
     (transparent-wrap-test.wrapping:required-optional 1 2 3 4 5))))
 
+(deftest test-required-optional-aux ()
+  (is
+   (equal
+    (transparent-wrap-test.original:required-optional-aux 1 2)
+    (transparent-wrap-test.wrapping:required-optional-aux 1 2)))
+  (is
+   (equal
+    (transparent-wrap-test.original:required-optional-aux 1 2 3)
+    (transparent-wrap-test.wrapping:required-optional-aux 1 2 3)))
+  (is
+   (equal
+    (transparent-wrap-test.original:required-optional-aux 1 2 3 4)
+    (transparent-wrap-test.wrapping:required-optional-aux 1 2 3 4)))
+  (is
+   (equal
+    (transparent-wrap-test.original:required-optional-aux 1 2 3 4 5)
+    (transparent-wrap-test.wrapping:required-optional-aux 1 2 3 4 5))))
+
 #.
 `(deftest test-required-optional-rest ()
    ,@(let ((arguments (list 1 2 3 4 5 6 7)))
@@ -43,8 +73,24 @@
              collect
                `(is
                  (equal
-                  (transparent-wrap-test.original:required-optional-rest ,@arglist)
-                  (transparent-wrap-test.wrapping:required-optional-rest ,@arglist))))))
+                  (transparent-wrap-test.original:required-optional-rest
+                   ,@arglist)
+                  (transparent-wrap-test.wrapping:required-optional-rest
+                   ,@arglist))))))
+
+#.
+`(deftest test-required-optional-rest-aux ()
+   ,@(let ((arguments (list 1 2 3 4 5 6 7)))
+          (loop for arglist in
+                 (loop for i from 2 to (length arguments)
+                    collect (subseq arguments 0 i))
+             collect
+               `(is
+                 (equal
+                  (transparent-wrap-test.original:required-optional-rest-aux
+                   ,@arglist)
+                  (transparent-wrap-test.wrapping:required-optional-rest-aux
+                   ,@arglist))))))
 
 #.
 `(deftest test-required-optional-rest-key ()
@@ -55,8 +101,10 @@
              collect
                `(is
                  (equal
-                  (transparent-wrap-test.original:required-optional-rest-key ,@arglist)
-                  (transparent-wrap-test.wrapping:required-optional-rest-key ,@arglist)))))
+                  (transparent-wrap-test.original:required-optional-rest-key
+                   ,@arglist)
+                  (transparent-wrap-test.wrapping:required-optional-rest-key
+                   ,@arglist)))))
    ,@(loop for combination in
           (let ((combinations)
                 (arguments '((:f 6) (:g 7) (:h 8) (:z 9) (:y 10) (:x 11))))
@@ -75,7 +123,7 @@
               1 2 3 4 5 ,@combination)))))
 
 #.
-`(deftest test-required-optional-key ()
+`(deftest test-required-optional-rest-key-aux ()
    ,@(let ((arguments (list 1 2 3 4 5)))
           (loop for arglist in
                  (loop for i from 2 to (length arguments)
@@ -83,8 +131,10 @@
              collect
                `(is
                  (equal
-                  (transparent-wrap-test.original:required-optional-key ,@arglist)
-                  (transparent-wrap-test.wrapping:required-optional-key ,@arglist)))))
+                  (transparent-wrap-test.original:required-optional-rest-key-aux
+                   ,@arglist)
+                  (transparent-wrap-test.wrapping:required-optional-rest-key-aux
+                   ,@arglist)))))
    ,@(loop for combination in
           (let ((combinations)
                 (arguments '((:f 6) (:g 7) (:h 8) (:z 9) (:y 10) (:x 11))))
@@ -97,8 +147,70 @@
           collect
           `(is
             (equal
-             (transparent-wrap-test.original:required-optional-key 1 2 3 4 5 ,@combination)
-             (transparent-wrap-test.wrapping:required-optional-key 1 2 3 4 5 ,@combination)))))
+             (transparent-wrap-test.original:required-optional-rest-key-aux
+              1 2 3 4 5 ,@combination)
+             (transparent-wrap-test.wrapping:required-optional-rest-key-aux
+              1 2 3 4 5 ,@combination)))))
+
+#.
+`(deftest test-required-optional-key ()
+   ,@(let ((arguments (list 1 2 3 4 5)))
+          (loop for arglist in
+                 (loop for i from 2 to (length arguments)
+                    collect (subseq arguments 0 i))
+             collect
+               `(is
+                 (equal
+                  (transparent-wrap-test.original:required-optional-key
+                   ,@arglist)
+                  (transparent-wrap-test.wrapping:required-optional-key
+                   ,@arglist)))))
+   ,@(loop for combination in
+          (let ((combinations)
+                (arguments '((:f 6) (:g 7) (:h 8) (:z 9) (:y 10) (:x 11))))
+            (loop for i from 0 to (length arguments)
+                 do
+                 (map-combinations
+                  (lambda (c) (push c combinations))
+                  arguments :length i))
+            (reverse (mapcar #'flatten combinations)))
+          collect
+          `(is
+            (equal
+             (transparent-wrap-test.original:required-optional-key
+              1 2 3 4 5 ,@combination)
+             (transparent-wrap-test.wrapping:required-optional-key
+              1 2 3 4 5 ,@combination)))))
+
+#.
+`(deftest test-required-optional-key-aux ()
+   ,@(let ((arguments (list 1 2 3 4 5)))
+          (loop for arglist in
+                 (loop for i from 2 to (length arguments)
+                    collect (subseq arguments 0 i))
+             collect
+               `(is
+                 (equal
+                  (transparent-wrap-test.original:required-optional-key-aux
+                   ,@arglist)
+                  (transparent-wrap-test.wrapping:required-optional-key-aux
+                   ,@arglist)))))
+   ,@(loop for combination in
+          (let ((combinations)
+                (arguments '((:f 6) (:g 7) (:h 8) (:z 9) (:y 10) (:x 11))))
+            (loop for i from 0 to (length arguments)
+                 do
+                 (map-combinations
+                  (lambda (c) (push c combinations))
+                  arguments :length i))
+            (reverse (mapcar #'flatten combinations)))
+          collect
+          `(is
+            (equal
+             (transparent-wrap-test.original:required-optional-key-aux
+              1 2 3 4 5 ,@combination)
+             (transparent-wrap-test.wrapping:required-optional-key-aux
+              1 2 3 4 5 ,@combination)))))
 
 #.
 `(deftest test-required-key ()
@@ -114,8 +226,29 @@
           collect
           `(is
             (equal
-             (transparent-wrap-test.original:required-key 1 2 ,@combination)
-             (transparent-wrap-test.wrapping:required-key 1 2 ,@combination)))))
+             (transparent-wrap-test.original:required-key
+              1 2 ,@combination)
+             (transparent-wrap-test.wrapping:required-key
+              1 2 ,@combination)))))
+
+#.
+`(deftest test-required-key-aux ()
+   ,@(loop for combination in
+          (let ((combinations)
+                (arguments '((:c 3) (:d 4) (:e 5) (:z 6) (:y 7) (:x 8))))
+            (loop for i from 0 to (length arguments)
+                 do
+                 (map-combinations
+                  (lambda (c) (push c combinations))
+                  arguments :length i))
+            (reverse (mapcar #'flatten combinations)))
+          collect
+          `(is
+            (equal
+             (transparent-wrap-test.original:required-key-aux
+              1 2 ,@combination)
+             (transparent-wrap-test.wrapping:required-key-aux
+              1 2 ,@combination)))))
 
 #.
 `(deftest test-required-rest-key ()
@@ -131,5 +264,26 @@
           collect
           `(is
             (equal
-             (transparent-wrap-test.original:required-rest-key 1 2 ,@combination)
-             (transparent-wrap-test.wrapping:required-rest-key 1 2 ,@combination)))))
+             (transparent-wrap-test.original:required-rest-key
+              1 2 ,@combination)
+             (transparent-wrap-test.wrapping:required-rest-key
+              1 2 ,@combination)))))
+
+#.
+`(deftest test-required-rest-key-aux ()
+   ,@(loop for combination in
+          (let ((combinations)
+                (arguments '((:c 3) (:d 4) (:e 5) (:z 6) (:y 7) (:x 8))))
+            (loop for i from 0 to (length arguments)
+                 do
+                 (map-combinations
+                  (lambda (c) (push c combinations))
+                  arguments :length i))
+            (reverse (mapcar #'flatten combinations)))
+          collect
+          `(is
+            (equal
+             (transparent-wrap-test.original:required-rest-key-aux
+              1 2 ,@combination)
+             (transparent-wrap-test.wrapping:required-rest-key-aux
+              1 2 ,@combination)))))
