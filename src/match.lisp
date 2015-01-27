1369e5bf2a1e1cc67c98180baad643add1814cbd
DalekBaldwin <DalekBaldwin@users.noreply.github.com>
2015-01-27 15:51:10 -0800

    add pattern matching for specialized lambda lists

--- src/match.lisp
+++ src/match.lisp
@@ -1,6 +1,7 @@
 (in-package :transparent-wrap)
 (named-readtables:in-readtable :fare-quasiquote)
 
+;;; ordinary lambda lists
 ;;; http://www.lispworks.com/documentation/HyperSpec/Body/03_da.htm
 ;;; ---------------------------------------------------------------
 ;;; lambda-list::= (var*
@@ -11,8 +12,24 @@
 ;;;                   [&allow-other-keys]]
 ;;;                [&aux {var | (var [init-form])}*])
 
+;;; specialized lambda lists
+;;; http://www.lispworks.com/documentation/HyperSpec/Body/03_dc.htm
+;;; ---------------------------------------------------------------
+;;; lambda-list::= ({var | (var [specializer])}*
+;;;                 [&optional {var | (var [init-form [supplied-p-parameter]])}*]
+;;;                 [&rest var]
+;;;                 [&key {var | ({var | (keyword-name var)}
+;;;                                [init-form [supplied-p-parameter]])}*
+;;;                    [&allow-other-keys]]
+;;;                 [&aux {var | (var [init-form])}*]) 
+
 (defstruct required-param
   name)
+
+(defstruct specialized-param
+  whole
+  name
+  specializer)
 
 (defstruct optional-param
   whole
@@ -41,6 +58,15 @@
 (defpattern ?* (&rest patterns)
   `(or nil
        (list* ,@patterns)))
+
+(defun match-specialized (param)
+  (match param
+    ((or (list* name (?* specializer nil))
+         name)
+     (make-specialized-param
+      :whole param
+      :name name
+      :specialized specializer))))
 
 (defun match-optional (param)
   (match param
@@ -80,6 +106,10 @@
 (defun match-lambda-list (params)
   (match-requireds params))
 
+;; todo: generate wrappers for every method or have a catch-all policy
+(defun match-specialized-lambda-list (params)
+  (match-specialized-requireds (params)))
+
 (defun match-requireds (params)
   (match params
     (`(&optional ,@x)
@@ -92,6 +122,21 @@
       (match-auxes x))
     (`(,x ,@y)
       (list* (make-required-param :name x) (match-requireds y)))
+    (nil
+     nil)))
+
+(defun match-specialized-requireds (params)
+  (match params
+    (`(&optional ,@x)
+      (match-optionals x))
+    (`(&rest ,rest ,@x)
+      (list* (make-rest-param :name rest) (match-post-rest x)))
+    (`(&key ,@x)
+      (match-keys x))
+    (`(&aux ,@x)
+      (match-auxes x))
+    (`(,x ,@y)
+      (list* (match-specialized x) (match-specialized-requireds y)))
     (nil
      nil)))
 
